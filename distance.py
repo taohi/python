98c2679fe7631b27125b2cda3e64489adaa3f6cd
whatcanyou <501256595@qq.com>
2013-07-31 02:22:01 +0000

    modify distance.py

--- distance.py
+++ distance.py
@@ -4,24 +4,20 @@
 #1公里范围误差1米左右 
 import math
 import sys
-def rad (degree):
-    '''角度换算数 '''
-    radian = degree * math.pi / 180.0
-    return radian
 
 def distance (latA,lonA,latB,lonB):
     earth_radius = 6378137.0
-    radlatA = rad(latA)
-    radlonA = rad(lonA)
-    radlatB = rad(latB)
-    radlonB = rad(lonB)
+    radlatA = math.radians(latA)
+    radlonA = math.radians(lonA)
+    radlatB = math.radians(latB)
+    radlonB = math.radians(lonB)
     diffLat = radlatA - radlatB
     diffLon = radlonA - radlonB
     distan = 2 * math.asin(math.sqrt(math.pow(math.sin(diffLat/2),2) + math.cos(radlatA)*math.cos(radlatB)*math.pow(math.sin(diffLon/2),2)))
     distan =round(distan * earth_radius ,2)
     return distan
 
-
+# Two Examples to call the function.
 #print distance(float(sys.argv[1]) , float(sys.argv[2]),float(sys.argv[3]),float(sys.argv[4]))
 print distance(30.51372 , 114.42658,30.50929,114.42150)
 print distance(30.50929,114.42150,30.5766,111.8457)
