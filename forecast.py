5607db70040f496bc275cb686c62b79b0ad17e35
huanghaitao <tao_08@qq.com>
2014-02-13 18:23:54 +0800

    forecast.py:强制发短信

--- forecast.py
+++ forecast.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 #-*-coding:utf-8 -*-
-import PyWapFetion
+from PyWapFetion import Fetion
 import feedparser
+myfetion=Fetion('15171456231','eric111235')
 weather_source = "http://weather.raychou.com/?/detail/57466/rss"
 d=feedparser.parse(weather_source)
 send_content = d.feed.title + "\n明天:" + d.entries[1].description + "\n后天:" +  d.entries[2].description
 #print send_content
-PyWapFetion.send("15171456231","eric111235","15171456231",send_content)
+myfetion.send("15971653106",send_content,sm=True)
+myfetion.logout()
