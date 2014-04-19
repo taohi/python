37aa63f263b9517d1a61bc8e3ef76a13a6633757
huang haitao <tao_08@qq.com>
2014-04-19 19:08:13 +0800

    modify selection_sort.py

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -8,10 +8,11 @@ def selection_sort(seq):
             if seq[j]>seq[max_j]:
                 max_j=j
                 seq[i],seq[max_j]=seq[max_j],seq[i]
+            #print seq
     return seq
 
 def test_selection_sort():
-    seq=[3,5,2,6,8,1,0,3,5,6,2]
+    seq=[3,5,2,4,8,1]#,0,3,5,6,2]
     assert(selection_sort(seq)==sorted(seq))
     print("Test passed!")
 
