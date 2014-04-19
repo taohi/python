b6d1a1251804059c601eaeb2ace65813437101b3
huang haitao <tao_08@qq.com>
2014-04-19 11:36:40 +0800

    插入排序的python代码

---
+++ sorting/selection_sort.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#encoding=utf-8
+
+def selection_sort(seq):
+    for i in range(len(seq)-1,0,-1):
+        max_j=i
+        for j in range(max_j):
+            if seq[j]>seq[max_j]:
+                max_j=j
+                seq[i],seq[max_j]=seq[max_j],seq[i]
+    return seq
+
+def test_selection_sort():
+    seq=[3,5,2,6,8,1,0,3,5,6,2]
+    assert(selection_sort(seq)==sorted(seq))
+    print("Tests passed!")
+
+if __name__=="__main__":
+    test_selection_sort()
