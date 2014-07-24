e6bfabfdf9a075401bcd1536eadbb09d84bfcf37
taohi <taohi@taohi.net>
2014-07-24 20:42:12 +0800

    修改phone_locate.py:利用web service查询号码归属地

--- phone_locate.py
+++ phone_locate.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 #encoding:utf-8
 '''利用suds模块，使用web service，查询号码归属地 '''
+'''用法:$./phone_locate.py 1517xxxxx'''
+import sys
 import suds
 url = 'http://webservice.webxml.com.cn/WebServices/MobileCodeWS.asmx?wsdl'
 client = suds.client.Client(url)
 #print client
-result =client.service.getMobileCodeInfo(18086111076)  
+phone_num=sys.argv[1]
+result =client.service.getMobileCodeInfo(phone_num)  
 print result
 #print client.last_received
